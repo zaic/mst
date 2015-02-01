b41dc854a27669c7ee5c762455c76f56f489e439
Vadim Zaytsev <zaic101@gmail.com>
2015-02-01 15:02:41 +0600

    Remove local files from send script.

--- send.sh
+++ send.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -f src.tar.xz
-tar cJf src.tar.xz *.cpp *.h third_party_generator
+tar cJf src.tar.xz *.cpp *.h third_party_generator/*.h third_party_generator/*.cpp third_party_generator/Makefile
 scp src.tar.xz xeonphi:./z/mst
