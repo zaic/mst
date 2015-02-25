29fe604256b1baa5a7d8e57da6fc3d8481b1c13e
Vadim Zaytsev <zaic101@gmail.com>
2015-02-25 22:25:25 +0600

    Send GraphHPC directory only once.

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f src.tar.xz
-tar cJf src.tar.xz *.cpp *.h third_party_generator/*.h third_party_generator/*.cpp third_party_generator/Makefile GraphHPC
+tar cJf src.tar.xz *.cpp *.h third_party_generator/*.h third_party_generator/*.cpp third_party_generator/Makefile #GraphHPC
 scp src.tar.xz xeonphi:./z/mst
 scp src.tar.xz nicevt:./task
 
