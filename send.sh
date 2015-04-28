bb917f126dbe615e7e9fe25871d5d688fbda800b
Vadim Zaytsev <zaic101@gmail.com>
2015-04-29 03:32:03 +0600

    Remove contest cluset from send script.

--- send.sh
+++ send.sh
@@ -3,5 +3,5 @@
 rm -f src.tar.xz
 tar cJf src.tar.xz *.cpp *.h third_party_generator/*.h third_party_generator/*.cpp third_party_generator/Makefile #GraphHPC
 scp src.tar.xz xeonphi:./z/mst
-scp src.tar.xz nicevt:./task
+#scp src.tar.xz nicevt:./task
 
