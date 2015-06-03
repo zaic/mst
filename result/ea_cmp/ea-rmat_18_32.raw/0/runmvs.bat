590e5721943592dbeb156b8821d8c0778e6ed7f6
Vadim Zaytsev <zaic101@gmail.com>
2015-06-03 23:41:17 +0600

    Add more results for presentation.

---
+++ result/ea_cmp/ea-rmat_18_32.raw/0/runmvs.bat
@@ -0,0 +1,133 @@
+#!/bin/bash
+export CC="icc"
+export CPATH="/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/include:/opt/software/intel/composer_xe_2013_sp1.1.106/tbb/include"
+export CPLUS_INCLUDE_PATH="/nethome/kalgin/local/include:/nethome/kalgin/local/include:"
+export CVS_RSH="ssh"
+export CXX="icpc"
+export C_INCLUDE_PATH="/nethome/kalgin/local/include:/nethome/kalgin/local/include:"
+export F90="ifort"
+export FC="ifort"
+export GDBSERVER_MIC="/opt/software/intel/composer_xe_2013_sp1.1.106/debugger/gdb/target/mic/bin/gdbserver"
+export G_BROKEN_FILENAMES="1"
+export HISTCONTROL="ignoredups"
+export HISTSIZE="1000"
+export HOME="/nethome/kalgin"
+export HOSTNAME="login"
+export IDB="idb"
+export IDB_HOME="/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64"
+export INCLUDE="/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/include"
+export INTEL_LICENSE_FILE="/opt/software/intel/flexlm"
+export IPPROOT="/opt/software/intel/composer_xe_2013_sp1.1.106/ipp"
+export I_MPI_CC="icc"
+export I_MPI_F77="ifort"
+export I_MPI_F90="ifort"
+export I_MPI_FC="ifort"
+export I_MPI_MIC_POSTFIX=".mic"
+export I_MPI_MIC_PREFIX=""
+export I_MPI_ROOT="/opt/software/intel/impi/4.1.0.030"
+export LANG="en_US.UTF-8"
+export LD_LIBRARY_PATH="/nethome/kalgin/local/lib:/nethome/kalgin/local/lib:/opt/software/intel/impi/4.1.0.030/intel64/lib:/opt/software/intel/composer_xe_2013_sp1.1.106/tbb/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/compiler/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/ipp/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/compiler/lib/intel64:/opt/intel/mic/coi/host-linux-release/lib:/opt/intel/mic/myo/lib:/opt/software/intel/composer_xe_2013_sp1.1.106/mpirt/lib/intel64:/usr/linux-k1om-4.7/linux-k1om/usr/lib64/"
+export LESSOPEN="|/usr/bin/lesspipe.sh %s"
+export LIBRARY_PATH="/nethome/kalgin/local/lib:/nethome/kalgin/local/lib:/opt/software/intel/composer_xe_2013_sp1.1.106/compiler/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/ipp/lib/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/tbb/lib/intel64"
+export LOADEDMODULES="null:compilers/composer_xe/latest:parallel/mpi.intel/latest:launcher/suppz"
+export LOGNAME="kalgin"
+export LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=01;05;37;41:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.tbz=01;31:*.tbz2=01;31:*.bz=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=01;36:*.au=01;36:*.flac=01;36:*.mid=01;36:*.midi=01;36:*.mka=01;36:*.mp3=01;36:*.mpc=01;36:*.ogg=01;36:*.ra=01;36:*.wav=01;36:*.axa=01;36:*.oga=01;36:*.spx=01;36:*.xspf=01;36:"
+export MAIL="/var/spool/mail/kalgin"
+export MANPATH="/opt/xcat/share/man:/opt/software/intel/impi/4.1.0.030/man:/opt/software/intel/composer_xe_2013_sp1.1.106/man/en_US:/usr/share/man"
+export MIC_LD_LIBRARY_PATH="/opt/software/intel/composer_xe_2013_sp1.1.106/compiler/lib/mic:/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/lib/mic:/opt/software/intel/composer_xe_2013_sp1.1.106/mpirt/lib/mic:/opt/intel/mic/coi/device-linux-release/lib:/opt/intel/mic/myo/lib:/opt/software/intel/composer_xe_2013_sp1.1.106/tbb/lib/mic"
+export MIC_LIBRARY_PATH="/opt/software/intel/composer_xe_2013_sp1.1.106/tbb/lib/mic"
+export MKLROOT="/opt/software/intel/composer_xe_2013_sp1.1.106/mkl"
+export MODULEPATH="/opt/Modules:/opt/basis/modules/Modules/\$MODULE_VERSION/modulefiles:/opt/Modules"
+export MODULESHOME="/opt/basis/modules/Modules/3.2.10"
+export MODULE_VERSION="3.2.10"
+export MODULE_VERSION_STACK="3.2.10"
+export MPIRUN_CMD="/common/runmvs/bin/run-batch"
+export MPIRUN_SPAWNER="run-impi-hydra"
+export MPIR_HOME="/opt/software/intel/impi/4.1.0.030/intel64"
+export NLSPATH="/opt/software/intel/composer_xe_2013_sp1.1.106/debugger/intel64/locale/%l_%t/%N:/opt/software/intel/composer_xe_2013_sp1.1.106/mkl/lib/intel64/locale/%l_%t/%N:/opt/software/intel/composer_xe_2013_sp1.1.106/ipp/lib/intel64/locale/%l_%t/%N:/opt/software/intel/composer_xe_2013_sp1.1.106/compiler/lib/intel64/locale/%l_%t/%N"
+export NXDIR="/usr/NX"
+export OLDPWD
+export PATH="/common/runmvs/local/bin:/common/runmvs/bin:/opt/xcat/bin:/opt/xcat/sbin:/usr/lib64/qt-3.3/bin:/usr/NX/bin:/opt/software/intel/impi/4.1.0.030/intel64/bin:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64_mic:/opt/software/intel/composer_xe_2013_sp1.1.106:/debugger/gdb/intel64/py26/bin:/opt/software/intel/composer_xe_2013_sp1.1.106:/debugger/gui/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/mpirt/bin/intel64:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin"
+export PERL_BADLANG="0"
+export PWD="/nethome/kalgin/z/mst"
+export QTDIR="/usr/lib64/qt-3.3"
+export QTINC="/usr/lib64/qt-3.3/include"
+export QTLIB="/usr/lib64/qt-3.3/lib"
+export RUNMVSDIR="/common/runmvs/bin"
+export RUNMVSLOCALDIR="/common/runmvs/local/bin"
+export SHELL="/bin/sh"
+export SHLVL="3"
+export SSH_ASKPASS="/usr/libexec/openssh/gnome-ssh-askpass"
+export SSH_CLIENT="84.237.53.28 39094 22"
+export SSH_CONNECTION="84.237.53.28 39094 83.149.214.101 22"
+export SSH_TTY="/dev/pts/7"
+export SUPPZ="1"
+export TBBROOT="/opt/software/intel/composer_xe_2013_sp1.1.106/tbb"
+export TERM="xterm"
+export USER="kalgin"
+export XCATHOST="head:3001"
+export XCATROOT="/opt/xcat"
+export _LMFILES_="/opt/basis/modules/Modules/3.2.10/modulefiles/null:/opt/basis/modules/Modules/3.2.10/modulefiles/compilers/composer_xe/latest:/opt/basis/modules/Modules/3.2.10/modulefiles/parallel/mpi.intel/latest:/opt/basis/modules/Modules/3.2.10/modulefiles/launcher/suppz"
+EnvCommandLine=(
+)
+
+
+
+num_nodes=1
+hosts_list="$2"
+exitstatus_file="$3"
+prefix="$4"
+np=1
+tview_flag=
+progname="$4 /nethome/kalgin/z/mst/run.xeon.sh"
+mpich_args=""
+cmdLineArgs=" 18"
+width_distribution=0
+numofnodes=1
+proc_per_node=
+
+
+MPIRUN_HOME=/opt/software/intel/impi/4.1.0.030/intel64/bin
+cd "/nethome/kalgin/z/mst"
+
+if [  "" ] ;then
+  gentmpname=`mktemp machines.XXXXXX` || exit 1
+  /common/runmvs/bin/genmachinefile "$hosts_list"  1 >$gentmpname
+  hosts_list=$gentmpname
+fi
+
+#trap 'if [ "$proc_per_node" ] ; then rm  ;fi ; if [ "" ] ; then rm $gentmpname ;fi ; exit ' 0 1 2 3 15
+#if [ "$proc_per_node" ] ; then rm  ;fi ;
+###trap ' if [ "" ] ; then rm $gentmpname ;fi ; exit ' 0 1 2 3 15
+
+EnvCommandLine=(  "${EnvCommandLine[@]}" MACHINEFILE_NAME=$hosts_list ) 
+    export LMUTIL=lmutil
+
+
+if [ x = x-totalview ]
+then
+    if ! check_license -c /common/totalview/license.dat -f TV/Base
+    then
+        sleep 1
+        echo 215 >$exitstatus_file
+        exit 215
+    fi
+fi
+
+
+#/mpirun.ch_gm --gm-np $num_nodes --gm-f $tmpname --gm-kill 5  /nethome/kalgin/z/mst/run.xeon.sh  18
+#/mpirun.ch_gm -pg  $tmpname  "PATH=/common/runmvs/local/bin:/common/runmvs/bin:/opt/xcat/bin:/opt/xcat/sbin:/usr/lib64/qt-3.3/bin:/usr/NX/bin:/opt/software/intel/impi/4.1.0.030/intel64/bin:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64_mic:/opt/software/intel/composer_xe_2013_sp1.1.106:/debugger/gdb/intel64/py26/bin:/opt/software/intel/composer_xe_2013_sp1.1.106:/debugger/gui/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/bin/intel64:/opt/software/intel/composer_xe_2013_sp1.1.106/mpirt/bin/intel64:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin"  /nethome/kalgin/z/mst/run.xeon.sh  18
+#/common/runmvs/bin/run-batch  -np 1 -hostfile $tmpname  /nethome/kalgin/z/mst/run.xeon.sh  18
+. /common/runmvs/bin/run-batch
+
+###if [ "$proc_per_node" ] ; then rm  ;fi
+
+exitstatus=$?
+#echo exitatus=$exitstatus
+
+if [ 0 -ne 0 ] ; then
+    exitstatus=215
+fi
+#if [ "" ] ; then rm $gentmpname ;fi
+echo $exitstatus >$exitstatus_file
+exit $exitstatus
